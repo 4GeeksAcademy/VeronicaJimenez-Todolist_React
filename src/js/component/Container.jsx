cd39bf674932bb52137b0d329d1ff8b3b3aa51a6
Verónica Jiménez <128405596+VeronicaJimenez@users.noreply.github.com>
2023-07-03 23:18:05 +0000

    Falta funcionalidad de botones y estilo

--- src/js/component/Container.jsx
+++ src/js/component/Container.jsx
@@ -42,56 +42,7 @@ const Container = (props) => {
 		</div>
 	);
 	
-	/*const [newItem, setNewItem] = useState("");
-	const [items, setItems] = useState([]);
-  
-	function addItem() {
-	    
-	  const item = {
-		id: Math.floor(Math.random() * 1000),
-		value: newItem
-	  };
-  
-	  setItems(oldList => [...oldList, item]);
-	  setNewItem("");
-	}
-  
-	function deleteItem(id) {
-	  const newArray = items.filter(item => item.id !== id);
-	  setItems(newArray);
-	}
-  
-	return (
-	  <div className="TodoList">
-		<h1>To Do List!</h1>
-		<input
-		  type="text"
-		  placeholder="Add your task"
-		  value={newItem}
-		  onChange={e => setNewItem(e.target.value)}
-		  onKeyDown={event => {
-			if (event.key === "Enter") {
-			  addItem();
-			}
-		  }}
-		/>
-		<button onClick={addItem}>Add</button>
-		<ul>
-		  {items.map(item => (
-			<li key={item.id}>
-			  {item.value}{" "}
-			  <button
-				className="delete-button"
-				onClick={() => deleteItem(item.id)}
-			  >
-				X
-			  </button>
-			</li>
-		  ))}
-		</ul>
-		<p>Usted tiene {items.length} tarea(s) pendiente(s).</p>
-	  </div>
-	);*/
+	
 
 };
 
